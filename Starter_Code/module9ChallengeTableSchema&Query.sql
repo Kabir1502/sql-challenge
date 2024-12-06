cfba7d01248baa675b05a6327c330267b60e0eee
Kabir1502 <kabir.mahapatra275@gmail.com>
2024-12-06 14:25:45 -0800

    SQL File containing Queries and Table Schemata

---
+++ Starter_Code/module9ChallengeTableSchema&Query.sql
@@ -0,0 +1,104 @@
+CREATE TABLE departments (
+    dept_no VARCHAR(10) NOT NULL PRIMARY KEY, 
+    dept_name VARCHAR(40) NOT NULL   
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,              
+    dept_no VARCHAR(10) NOT NULL,                               
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL,              
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+DROP TABLE dept_manager;
+
+CREATE TABLE employees (
+    emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,         
+    first_name VARCHAR(14) NOT NULL,  
+    last_name VARCHAR(16) NOT NULL,   
+    sex CHAR(1) NOT NULL,             
+    hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,              
+    salary INT NOT NULL,             
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL
+);
+
+
+
+
+
+SELECT * 
+FROM departments
+
+SELECT * 
+FROM dept_emp
+
+SELECT * 
+FROM dept_manager
+
+SELECT * 
+FROM employees
+
+SELECT * 
+FROM titles
+
+SELECT *
+FROM salaries
+
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN employees e ON de.emp_no = e.emp_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM dept_emp de
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
